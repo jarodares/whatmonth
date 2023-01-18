61b9bcb35e4ac3ca7cce46f5616c55e1761650cc
jarodares <jrob.valladares@gmail.com>
2023-01-17 22:41:37 -0600

    DOM functionality added

---
+++ main.js
@@ -0,0 +1,65 @@
+// --------- TODO  ----------------
+// SETMATRIX FUNCTION
+// DEFAULT VALUES WHEN NOT SELECTED
+// PLUS AND MINUS BUTTONS
+
+const btn = document.getElementById('set');
+const dayelement = document.getElementById('days');
+const monthelement = document.getElementById('month')
+const monthCell = document.getElementsByTagName('td')
+const plusbtn = document.getElementById('plusBtn')
+const minusBtn = document.getElementById('minusBtn')
+var multiplier = 0
+
+function getCurrent(){
+    return [monthelement.selectedIndex,dayelement.selectedIndex]
+}
+
+function setMatrix(dayelement){
+    if(dayelement == 0 ){dayelement=1}
+    var montharr = setdays(monthCell,monthelement.selectedIndex)
+    let week = dayelement*7 
+    let j = multiplier * 12
+    for(let i = 0; i<monthCell.length;i++){
+        montharr[i].textContent = (j * week)
+        j++;
+    }
+}
+
+function setdays(collection,index){
+    var arr = Array.from(collection) 
+    for(let i = 0; i<index-1; i++){
+        let a = arr.shift();
+        arr.push(a)
+    }
+    return arr
+}
+
+btn.addEventListener('click', function(){
+
+    for(let i = 0; i<monthCell.length; i++){
+        monthCell[i].textContent="-";
+    }
+})
+
+monthelement.addEventListener("change", function(){
+    setMatrix(dayelement.selectedIndex);
+});
+
+dayelement.addEventListener('change', function(){
+    setMatrix(dayelement.selectedIndex)
+})
+
+plusbtn.addEventListener('click', function(){
+    multiplier = multiplier+1
+    setMatrix(dayelement.selectedIndex)
+})
+
+minusBtn.addEventListener('click',function(){
+    if(!multiplier < 1){
+        multiplier = multiplier-1
+        setMatrix(dayelement.selectedIndex)
+    }
+})
+
+
